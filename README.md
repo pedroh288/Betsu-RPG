35690065cc18e1bef5a13441a948609f454a0b8f
Pedro H. Rangel <pedrohrangela@gmail.com>
2026-06-03 00:54:20 -0300

    Mudança de cores

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Betsu-RPG
+Projeto desenvolvido em Python para auxiliar na administração e organização do RPG Betsu, automatizando diversas mecânicas importantes da mesa, como cálculos de atributos, progressão de status, gerenciamento de recursos e geração procedural de criaturas.
+
+O sistema foi estruturado para tornar as sessões mais rápidas e imersivas, reduzindo a necessidade de cálculos manuais durante o jogo.
+Dentre as automatizações estão:
+## Sistema de Spawn
+Baseados em probabilidade, raridade, região e horário, permitindo que diferentes criaturas apareçam de forma dinâmica e contextual dentro do universo do RPG.
+Toda a lógica foi organizada de forma escalável, facilitando a expansão futura do sistema com novos monstros, regiões, mecânicas e regras.
+## Sistema de Atributos
+Cálculos automáticos para atributos dos personagens, incluindo vida, mana, stamina, resistências físicas e mágicas, movimentação, alcance e progressão por nível.
--- status.py
+++ status.py
@@ -18,34 +18,34 @@ nivel = int(input('Agora, me diga o seu nível (começando do nível 1): '))
 
 N = nivel - 1
 
-print('Vamos calcular os seus status agora: ')
+print('Vamos calcular os seus \033[34mstatus\033[0m agora: ')
 mana = 20 + (5 * I) + (10 * N)  # +10 de mana por nível
 vida = 20 + (5 * Co) + (5 * N)   # +5 de vida por nível
 stamina = 20 + 5 * (A + F) + (10 * N)  # +10 de stamina por nível
 Fr = ((I*Ca)*nivel)
 
 print('='*20)
-print('Sua MANA inicial é: \033[32m{}\033[0m'.format(mana))
-print('Sua VIDA inicial: \033[32m{}\033[0m'.format(vida))
-print('Sua STAMINA é: \033[32m{}\033[0m'.format(stamina))
-print('Seu FIO DE RAZÃO é \033[32m{}\033[0m'.format(Fr))
+print('Sua \33[36mMANA\33[0m inicial é: \033[32m{}\033[0m'.format(mana))
+print('Sua \33[36mVIDA\33[0m inicial: \033[32m{}\033[0m'.format(vida))
+print('Sua \33[36mSTAMINA\33[0m é: \033[32m{}\033[0m'.format(stamina))
+print('Seu \33[36mFIO DE RAZÃO\33[0m é \033[32m{}\033[0m'.format(Fr))
 print('='*20)
 
-print('Agora sobre suas resistência...')
+print('Agora sobre suas \033[34mresistência\033[0m...')
 rf = Co + F * 2
 rm = Co + I
 
 print('='*20)
-print('Sua RESISTÊNCIA FÍSICA é: \033[32m{}\033[0m'.format(rf))
-print('Sua RESISTÊNCIA MÁGICA é: \033[32m{}\033[0m'.format(rm))
+print('Sua \033[36mRESISTÊNCIA FÍSICA\033[0m é: \033[32m{}\033[0m'.format(rf))
+print('Sua \033[36mRESISTÊNCIA MÁGICA\33[0m é: \033[32m{}\033[0m'.format(rm))
 print('='*20)
 
-print('Agora sobre sobre os alcances...')
+print('Agora sobre sobre os \033[34malcances\033[0m...')
 m = 3 + A
 am = 4 + I
 af = 1
 
 print('='*20)
-print('Sua MOVIMENTAÇÃO é: \033[32m{}\033[0m'.format(m))
-print('Seu ALCANCE MÁGICO é: \033[32m{}\033[0m'. format(am))
-print('Seu ALCANCE FÍSICO é: \033[32m{}\033[0m'.format(af))
+print('Sua \33[36mMOVIMENTAÇÃO\33[0m é: \033[32m{}\033[0m'.format(m))
+print('Seu \33[36mALCANCE MÁGICO\33[0m é: \033[32m{}\033[0m'. format(am))
+print('Seu \33[36mALCANCE FÍSICO\33[0m é: \033[32m{}\033[0m'.format(af))
